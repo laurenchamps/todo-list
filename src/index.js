870ee1836ca123a2df6aca7c33e77e6dc6d04a80
Lauren Champs <laurenkchamps@gmail.com>
2023-10-13 09:59:37 +1030

    Add function to remove task from project and UI

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import './style.css';
 import { formatDistance, subDays } from 'date-fns'; 
 import { createTask } from './tasks.js';
-import { createProject, addTaskToProject, displayTasks, removeTask } from './projects.js';
-import { eventListener } from './ui.js';
+import { createProject, addProject, projectList } from './projects.js';
+import './ui.js';
 
 export const toDoList = createProject('toDoList');
 
-const task1 = createTask('Cook dinner', 'Prep and cook soup', new Date(2023, 9, 14), 'low');
-const task2 = createTask('Go shopping', 'Buy stuff', new Date(2023, 9, 14), 'low');
-const task3 = createTask('Walk the dog', 'Take her to the oval', new Date(2023, 9, 14), 'med');
+console.log(toDoList);
+addProject(toDoList);
 
+console.log(projectList);
 
-export const project1 = createProject('project1');
-
-addTaskToProject(task1, project1);
-addTaskToProject(task2, project1);
-addTaskToProject(task3, project1);
-
-displayTasks(project1);
-
-console.log(task1);
-removeTask(task1, project1);
-
-console.log(project1);
+// const task1 = createTask('Cook dinner', 'Prep and cook soup', new Date(2023, 9, 14), 'low');
+// const task2 = createTask('Go shopping', 'Buy stuff', new Date(2023, 9, 14), 'low');
+// const task3 = createTask('Walk the dog', 'Take her to the oval', new Date(2023, 9, 14), 'med');
--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,5 @@
 import { createTask } from './tasks.js';
-import { addTaskToProject} from './projects.js';
+import { addTaskToProject, removeTask } from './projects.js';
 import { toDoList } from './index.js';
 
 const enterTask = document.querySelector('.add-task');
@@ -7,39 +7,55 @@ const addTask = document.querySelector('#add-btn');
 const taskList = document.querySelector('.tasks');
 
 function addTaskToDOM(newTask, project) {
-    console.log(newTask.title);
-    console.log(`task-${project.tasks.indexOf(newTask)}`);
-
     const li = document.createElement('li');
     const input = document.createElement('input');
     const label = document.createElement('label');
-    const taskId = `task-${project.tasks.indexOf(newTask)}`;
+    const taskId = getTaskId(newTask, project);
 
     li.setAttribute('class', 'task');
 
     input.setAttribute('type', 'checkbox');
-    input.setAttribute('id', taskId);
+    input.setAttribute('id', taskId.id);
 
-    label.setAttribute('for', taskId);
+    label.setAttribute('for', taskId.id);
     label.textContent = newTask.title;
 
     li.appendChild(input);
     li.appendChild(label);
-    li.appendChild(createDeleteBtn(newTask));
+    li.appendChild(createDeleteBtn(newTask, project));
 
     taskList.appendChild(li);
 }
 
-function createDeleteBtn(task) {
-    const deleteBtn = document.createElement('button');
+function getTaskId(task, project) {
+    return { 
+        id: `task-${project.tasks.indexOf(task) + 1}`,
+        task: task,
+        project: project,
+    }
+}
 
+function createDeleteBtn(task, project) {
+    const deleteBtn = document.createElement('button');
+    
     deleteBtn.setAttribute('aria-label', 'delete');
     deleteBtn.setAttribute('id', task.title);
-
+    
     const textNode = document.createTextNode('\u00D7');
     deleteBtn.appendChild(textNode);
+    
+    const taskId = getTaskId(task, project);
+    deleteBtn.addEventListener('click', () => {
+        clearTask(taskId);
+    });
 
     return deleteBtn;
+}
+
+function clearTask(taskId) {
+    const inputElement = document.getElementById(taskId.id);
+    inputElement.parentNode.remove();
+    removeTask(taskId.task, taskId.project);
 }
 
 function clearInput() {
@@ -47,8 +63,9 @@ function clearInput() {
 }
 
 // Event listeners
-addTask.addEventListener('click', (e) => {
+export const eventListener = addTask.addEventListener('click', (e) => {
     e.preventDefault();
+    console.log(e);
     const newTask = createTask(enterTask.value);
     addTaskToProject(newTask, toDoList);
     addTaskToDOM(newTask, toDoList);
